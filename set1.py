602e2ea2d58582a094603ac9993b19c8535e900b
Lightromax <calendara132@gmail.com>
2024-12-28 16:57:37 +0000

    Add files via upload

---
+++ set1.py
@@ -0,0 +1,7 @@
+planset={"mercury", "venus", "earth", "mars", "jupiter", "saturn", "uranus", "pluto"}
+for i in planset:
+    print(i)
+if "moon" in planset:
+    print("moon is in planetset/planset")
+else:
+    print("moon is not is planetset/planset")
